15c1efd7820dec10f247658ccaecfde616473866
Kickaha123 <ChristianKnecht98@gmail.com>
2017-10-26 14:36:53 +0200

    New Solution

---
+++ while/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            x = 10000;
+
+            while (x >= 2000)
+            {
+                Console.WriteLine("Betrag auswählen");
+                x -= Convert.ToInt32(Console.ReadLine());
+
+            }
+            Console.WriteLine("Out of Money");
+            Console.ReadKey();
+        }
+    }
+}
