f54de66f6372263671c0f984ed7673f9bde3f691
jhdog <doifhg@kdsjf.de>
2017-11-02 12:28:27 +0100

    tadäus

---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] arrNumber = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+            //foreach (int number in arrNumber)
+            //{
+            //    Console.WriteLine(number);
+            //}
+            //Console.ReadKey();
+
+
+            //char i;
+            //    do
+            //    {
+             //       i = Console.ReadKey().KeyChar;
+             //       Console.Clear();
+                    
+             //   }
+             //   while (!Char.IsNumber(i));
+
+            //
+            //
+            //string[] blackListPIN = new string[] { "0000", "1234", "9999" };
+            //string i;
+            //bool xd = true;
+            //do
+            //{
+            //    xd = true;
+            //    i = Console.ReadLine();
+            //    foreach (string help in blackListPIN)
+            //    {
+            //        if (i == help)
+            //        {
+            //            Console.WriteLine("in Blacklist");
+            //            xd = false;
+            //            break;
+            //        }
+            //    }
+            //} while (xd == false);
+            //Console.ReadKey();
+
+            Konto kt1 = new Konto(12);
+            Konto kt2 = new Konto(12);
+            Konto kt3 = new Konto();
+
+            Console.WriteLine(kt1.getKtoNr());
+            Console.WriteLine(kt2.getKtoNr());
+            Console.WriteLine(kt3.getKtoNr());
+            Console.ReadKey();
+
+            
+
+
+        }
+
+        class Konto
+        {
+            private uint ktoNr;
+            private static uint ktoNrplus = 400000;
+
+            private double ktoStand = 300;
+
+            public Konto()
+            {
+                ktoNr = ktoNrplus++;
+            }
+            public Konto(double ulul)
+            {
+                ktoNr = ktoNrplus++;
+            }
+            public uint getKtoNr()
+            {
+                return ktoNr;
+            }
+            public double getKtoStand()
+            {
+                return ktoStand;
+            }
+
+        }
+    }
+}
